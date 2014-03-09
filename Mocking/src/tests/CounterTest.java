86892f100d49786e0cdbce8db33b24cf47caacf1
Nick Sutton <nsutton78-at-gmail-com>
2014-03-09 20:48:28 +0000

    updated the tests to check all verify statements

--- Mocking/src/tests/CounterTest.java
+++ Mocking/src/tests/CounterTest.java
@@ -43,4 +43,83 @@ public class CounterTest {
 		
 		assertEquals("Wrong Answer!", first, mockedFirst);
 	}
+	
+	@Test
+	public void test3() {
+		
+		Integer first,mockedFirst;
+		
+		Counter count = new Counter();
+		
+		Counter mockedCounter = mock(Counter.class);
+		when(mockedCounter.getValue()).thenReturn(1).thenReturn(2).thenReturn(3);
+		
+		first = count.getValue();
+		mockedFirst = mockedCounter.getValue();
+		assertEquals("Wrong Answer!", first, mockedFirst);
+		
+		first = count.getValue();
+		mockedFirst = mockedCounter.getValue();
+		assertEquals("Wrong Answer!", first, mockedFirst);
+		
+		first = count.getValue();
+		mockedFirst = mockedCounter.getValue();		
+		
+		verify(mockedCounter,atLeast(3)).getValue();
+		assertEquals("Wrong Answer!", first, mockedFirst);
+		
+		mockedFirst = mockedCounter.getValue();	
+		verify(mockedCounter,atLeast(3)).getValue();
+	}
+	
+	@Test
+	public void atMostTest() {
+		
+		Integer mockedFirst;
+
+		Counter mockedCounter = mock(Counter.class);
+		when(mockedCounter.getValue()).thenReturn(1).thenReturn(2).thenReturn(3);
+		
+		mockedFirst = mockedCounter.getValue();
+		mockedFirst = mockedCounter.getValue();
+		mockedFirst = mockedCounter.getValue();
+		
+		verify(mockedCounter,atMost(3)).getValue();
+				
+		mockedFirst = mockedCounter.getValue();
+		//verify(mockedCounter,atMost(3)).getValue();
+	}
+	
+	@Test
+	public void atLeastOnceTest() {
+		
+		Integer mockedFirst = 0;
+
+		Counter mockedCounter = mock(Counter.class);
+		when(mockedCounter.getValue()).thenReturn(1).thenReturn(2).thenReturn(3);
+		
+		//verify(mockedCounter,atLeastOnce()).getValue();
+		
+		mockedFirst = mockedCounter.getValue();
+
+		verify(mockedCounter,atLeastOnce()).getValue();	
+		
+	}	
+	
+	@Test
+	public void neverTest() {
+		
+		Integer mockedFirst = 0;
+
+		Counter mockedCounter = mock(Counter.class);
+		when(mockedCounter.getValue()).thenReturn(1).thenReturn(2).thenReturn(3);
+		
+		verify(mockedCounter,never()).getValue();
+		
+		mockedFirst = mockedCounter.getValue();
+
+		verify(mockedCounter,never()).getValue();	
+		
+	}		
+	
 }
